f720b9acc41831abf620c2fb0a5712a5b823b8f6
Jason Harrelson <jason@lookforwardenterprises.com>
2009-03-16 18:09:59 -0500

    Changed order of checking as an error would have happened.

--- lib/auto_authenticable.rb
+++ lib/auto_authenticable.rb
@@ -16,8 +16,9 @@ module AutoAuthenticable
     def fake_login
       return unless Rails.env.development?
       return if self.current_user
-      return unless DEV_CONFIG[:do_auto_login]
       throw 'Failure doing fake login: cannot find DEV_CONFIG.' if DEV_CONFIG.nil?
+      return unless DEV_CONFIG[:do_auto_login]
+
       user = User.authenticate( DEV_CONFIG[:logged_in_user], DEV_CONFIG[:logged_in_password] )
 
       if user
